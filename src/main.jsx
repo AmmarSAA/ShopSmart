bfe0c64cc098c9426314fcba2f5530f1d7080a4d
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-07-09 14:30:27 +0500

    FIX: Added Context

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import ContextProvider from './Context/context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
   </React.StrictMode>,
 )
