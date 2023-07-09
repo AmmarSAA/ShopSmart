f5329a73bbc8dcb8ac86bdf0f1eb6f5dc50d1454
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-07-09 14:29:18 +0500

    FIX: Updated styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,15 +12,20 @@ import { BsCartPlusFill } from 'react-icons/bs'
 function ProductCard({product}) {
   return (
     <div className="col-md-6 my-4">
-      <Card>
-        <Card.Img variant="top" src={product.thumbnail} />
+      <div className="container">
+      <Card style={{width: '100%', height: '70vh'}}>
+        <Card.Img variant="top" src={product.thumbnail} style={{width: '100%', height: '40vh'}}/>
         <Card.Body>
           <Card.Title>{product.title}</Card.Title>
-          <Card.Text>{product.description}
+          <Card.Text>{product.description.length > 50 ?
+            `${product.description.substring(0, 55)} ...` : product.description
+          }
           </Card.Text>
+          <Card.Text> <b>Price: </b>{product.price}$ </Card.Text>
           <Link to={`/product/${product.id}`} className="btn btn-dark float-end"><BsCartPlusFill/></Link>
         </Card.Body>
       </Card>
+    </div>
     </div>
   );
 }
