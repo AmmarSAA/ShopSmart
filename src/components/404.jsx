bd203bba350b6b4dc3f792e764fa3d65b4da9c2d
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-07-22 14:22:26 +0500

    FIX: included sign in/up buttons,  minor ui fixes

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -6,7 +6,9 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
-import { BsExclamationCircle } from "react-icons/bs";
+import { BsExclamationCircle, BsHouse } from "react-icons/bs";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignUpForm";
 
 const NotFoundPage = () => {
   return (
@@ -14,7 +16,18 @@ const NotFoundPage = () => {
       <BsExclamationCircle size={80} color="#dc3545" />
       <h1 className="mt-4">Oops! Page Not Found</h1>
       <p className="text-center">The page you are looking for does not exist.</p>
-      <Link to="/" className="btn btn-primary mt-4">Go to Home</Link>
+      <div className="row col-3 p-2">
+        <Link to="/" type="button" class="btn custom-button d-flex align-items-center gap-2 btn btn-light">
+          <BsHouse className="text-light navbar-icon text-white" />
+          <span class="d-none d-lg-inline text-white"> Go to Home</span>
+        </Link>
+      </div>
+      <div className="row col-3 p-2">
+        <LoginForm />
+      </div>
+      <div className="row col-3 p-2">
+        <SignupForm />
+      </div>
     </div>
   );
 };
