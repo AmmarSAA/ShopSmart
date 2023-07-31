22bf5b4e354d6597f6dc6aedce81f8c0807fb87e
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-07-31 23:33:31 +0500

    FEAT: ShopSmart API (Server)

---
+++ server/api/brand/Routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const { getBrand, getBrandByName, createBrand, deleteBrand, updateBrand } = require('./Controller')
+
+router.get('/getBrand', getBrand)
+router.get('/getBrandByName', getBrandByName)
+router.post('/createBrand', createBrand)
+router.delete('/deleteBrand', deleteBrand)
+router.put('/updateBrand', updateBrand)
+
+
+
+module.exports = router
