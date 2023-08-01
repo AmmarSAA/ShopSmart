0b72b0282a7622162c288509df64823321acbda4
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-08-01 20:59:30 +0500

    FIX: made comments random

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ReactPaginate from "react-paginate";
 
+// Function to generate a random number between min (inclusive) and max (exclusive)
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 const Comment = () => {
   const [comments, setComments] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
 
-  const commentsPerPage = 5;
+  const commentsPerPage = 8;
   const pageCount = Math.ceil(comments.length / commentsPerPage);
 
   useEffect(() => {
     const fetchComments = async () => {
       try {
-        const response = await axios.get("https://dummyjson.com/comments");
+        const response = await axios.get(
+          `https://dummyjson.com/comments?skip=${getRandomNumber(
+            1,
+            100
+          )}&limit=50`
+        );
         // Extract the 'comments' array from the API response
         const commentsArray = response.data.comments;
         setComments(commentsArray);
@@ -40,7 +50,9 @@ const Comment = () => {
         displayedComments.map((comment) => (
           <div className="card mb-3" key={comment.id}>
             <div className="card-body">
-              <h5 className="card-title">User: {comment.user?.username || "Unknown"}</h5>
+              <h5 className="card-title">
+                User: {comment.user?.username || "Unknown"}
+              </h5>
               <p className="card-text">{comment.body}</p>
             </div>
           </div>
