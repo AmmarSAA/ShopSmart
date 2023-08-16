b97952757d7424f4a63fd8714b65ca98e570980b
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-08-16 14:03:08 +0500

    FIX: Added Search Bar

--- src/pages/admin/Brand.jsx
+++ src/pages/admin/Brand.jsx
@@ -6,12 +6,37 @@
 
 import React from 'react'
 import ShowBrands from '../../components/admin/ShowBrands'
+import { Form } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import { FaSearch } from 'react-icons/fa'
 
 export default function Brand() {
   return (
     <div className="container">
       <div className="justify-content-between align-items-center">
-        <div className="h4 p-3">All Brands</div>
+        <div className="row">
+          <div className="col-8">
+            <div className="h4 p-3">All Brands</div>
+          </div>
+          <div className="col-4">
+            <Form inline>
+              <Row>
+                <Col xs="auto">
+                  <Form.Control
+                    type="text"
+                    placeholder="Search"
+                    className="mt-1 p-2 pb-2 m-0"
+                  />
+                </Col>
+                <Col xs="auto">
+                  <Button type="submit" className='custom-button px-3 d-flex align-items-center'><FaSearch /></Button>
+                </Col>
+              </Row>
+            </Form>
+          </div>
+        </div>
         <ShowBrands />
       </div>
     </div>
