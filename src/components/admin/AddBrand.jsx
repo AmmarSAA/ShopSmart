83f36790d846a5f41f455ede0f2d892bd45dfafa
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-08-17 12:11:53 +0500

    FEAT: Add Brand

--- src/components/admin/AddBrand.jsx
+++ src/components/admin/AddBrand.jsx
@@ -1,20 +1,48 @@
-/****************************
-* File Name: AddBrand.jsx 	*
-* Author: Ammar S.A.A 			*
-* Output: Add Brand Modal   *
-****************************/
-
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import axios from "axios";
 import "./style.css";
 import UpdateBrand from "./UpdateBrand";
+import { storage } from "../../utils/firebaseConfig";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage"; // Add this import
 
 function AddBrand() {
   const [show, setShow] = useState(false);
+  const [brandName, setBrandName] = useState("");
+  const [brandImage, setBrandImage] = useState(null);
+
+  const addBrand = () => {
+    const formData = new FormData();
+    formData.append("Name", brandName);
+    formData.append("Image", brandImage);
+
+    const storageRef = ref(storage, `images/brand/${brandImage.name}`);
+    uploadBytes(storageRef, brandImage).then((snapshot) => {
+      getDownloadURL(snapshot.ref) // Import the getDownloadURL function
+        .then((url) => {
+          const payload = { Name: brandName, Image: url };
+          axios
+            .post(`https://shopsmart-api.cyclic.app/api/brand/createBrand`, payload)
+            .then((json) => {
+              setShow(false);
+            })
+            .catch((err) => alert(err.message));
+        })
+        .catch((error) => alert(error.message));
+    });
+  }
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleBrandNameChange = (event) => {
+    setBrandName(event.target.value);
+  }
+
+  const handleBrandImageChange = (event) => {
+    setBrandImage(event.target.files[0]);
+  }
 
   return (
     <>
@@ -25,24 +53,16 @@ function AddBrand() {
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Body className="d-flex align-items-center justify-content-center">
           <form className="form1">
-            <p className="title1">Brand </p>
-            <p className="message1">Add New Brand. </p>
-            {/* <div className="flex1">
-              <label>
-                <input className="input1" type="text" placeholder="" required="" />
-                <span>Firstname</span>
-              </label>
-              <label>
-                <input className="input1" type="text" placeholder="" required="" />
-                <span>Lastname</span>
-              </label>
-            </div> */}
+            <p className="title1">Brand</p>
+            <p className="message1">Add New Brand.</p>
             <label>
               <input
                 className="input1 pb-1"
                 type="text"
                 placeholder=""
-                required=""
+                required="{true}"
+                value={brandName}
+                onChange={handleBrandNameChange}
               />
               <span>Brand</span>
             </label>
@@ -51,13 +71,14 @@ function AddBrand() {
                 className="input1 pt-3 pb-1 form-control"
                 type="file"
                 placeholder=""
-                required=""
+                required="{true}"
+                onChange={handleBrandImageChange}
               />
               <span>Image</span>
             </label>
-            <button className="submit1">Add New</button>
+            <button className="submit1" onClick={addBrand}>Add New</button>
             <p className="signin1">
-              Wanna Update Brand? <a href="#" className="btn-sm"><UpdateBrand /></a>{" "}
+              Wanna Update Brand? <a href="#" className="btn-sm"><UpdateBrand /></a>
             </p>
           </form>
         </Modal.Body>
