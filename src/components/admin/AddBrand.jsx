f5cb1ed140bb722f2c60b8daf27f65a2c9dd0b2c
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-08-17 18:34:01 +0500

    FEAT: Add Brand Integrated API

--- src/components/admin/AddBrand.jsx
+++ src/components/admin/AddBrand.jsx
@@ -1,48 +1,75 @@
-import { useState } from "react";
+/****************************
+* File Name: AddBrand.jsx 	*
+* Author: Ammar S.A.A       *
+* Output: Add Brand Modal   *
+****************************/
+
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import axios from "axios";
-import "./style.css";
-import UpdateBrand from "./UpdateBrand";
 import { storage } from "../../utils/firebaseConfig";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage"; // Add this import
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import UpdateBrand from "./UpdateBrand";
 
 function AddBrand() {
   const [show, setShow] = useState(false);
   const [brandName, setBrandName] = useState("");
-  const [brandImage, setBrandImage] = useState(null);
+  const [brandImage, setBrandImage] = useState("");
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const addBrand = () => {
+    setIsLoading(true);
+
     const formData = new FormData();
     formData.append("Name", brandName);
-    formData.append("Image", brandImage);
 
-    const storageRef = ref(storage, `images/brand/${brandImage.name}`);
-    uploadBytes(storageRef, brandImage).then((snapshot) => {
-      getDownloadURL(snapshot.ref) // Import the getDownloadURL function
-        .then((url) => {
-          const payload = { Name: brandName, Image: url };
-          axios
-            .post(`https://shopsmart-api.cyclic.app/api/brand/createBrand`, payload)
-            .then((json) => {
-              setShow(false);
-            })
-            .catch((err) => alert(err.message));
-        })
-        .catch((error) => alert(error.message));
-    });
-  }
+    if (!brandImage || !brandName) {
+      setError("Please fill in all fields.");
+      setIsLoading(false);
+      return; // Exit the function if fields are not filled
+    }
+
+    if (brandImage && brandName) {
+      const storageRef = ref(storage, `images/brand/${brandImage.name}`);
+      uploadBytes(storageRef, brandImage).then((snapshot) => {
+        getDownloadURL(snapshot.ref)
+          .then((url) => {
+            formData.append("Image", url);
+            axios
+              .post(
+                `https://shopsmart-api.cyclic.app/api/brand/createBrand`,
+                formData
+              )
+              .then((json) => {
+                setMessage(json.data.message);
+              })
+              .catch((err) => setError(err.message))
+              .finally(() => {
+                setIsLoading(false);
+                setShow(false);
+              });
+          })
+          .catch((error) => {
+            setError(error.message);
+            setIsLoading(false);
+          });
+      });
+    }
+  };
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleBrandNameChange = (event) => {
     setBrandName(event.target.value);
-  }
+  };
 
   const handleBrandImageChange = (event) => {
     setBrandImage(event.target.files[0]);
-  }
+  };
 
   return (
     <>
@@ -53,6 +80,19 @@ function AddBrand() {
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Body className="d-flex align-items-center justify-content-center">
           <form className="form1">
+            {isLoading ? (
+              <h4 className="alert alert-info text-center text-capitalize">
+                Creating Brand...
+              </h4>
+            ) : message ? (
+              <h4 className="alert alert-success text-center text-capitalize">
+                {message}
+              </h4>
+            ) : error ? (
+              <h4 className="alert alert-danger text-center text-capitalize">
+                {error}
+              </h4>
+            ) : null}
             <p className="title1">Brand</p>
             <p className="message1">Add New Brand.</p>
             <label>
@@ -60,7 +100,7 @@ function AddBrand() {
                 className="input1 pb-1"
                 type="text"
                 placeholder=""
-                required="{true}"
+                required={true}
                 value={brandName}
                 onChange={handleBrandNameChange}
               />
@@ -71,14 +111,19 @@ function AddBrand() {
                 className="input1 pt-3 pb-1 form-control"
                 type="file"
                 placeholder=""
-                required="{true}"
+                required={true}
                 onChange={handleBrandImageChange}
               />
               <span>Image</span>
             </label>
-            <button className="submit1" onClick={addBrand}>Add New</button>
+            <button className="submit1" onClick={addBrand}>
+              Add New
+            </button>
             <p className="signin1">
-              Wanna Update Brand? <a href="#" className="btn-sm"><UpdateBrand /></a>
+              Wanna Update Brand?{" "}
+              <a href="#" className="btn-sm">
+                <UpdateBrand />
+              </a>
             </p>
           </form>
         </Modal.Body>
