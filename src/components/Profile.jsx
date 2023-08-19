4689dcfbdd31a2765f8b935c1f3f5327d8d2bb7a
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-08-19 18:12:42 +0500

    FIX: Added buttons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,23 +12,31 @@ const Profile = () => {
 
     return (
         <div className="container">
-            <h2>Profile Information</h2>
-            <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" id="name" value={state.userName} readOnly />
+            <h2 className='text-capitalize'>{state.userRole} information</h2>
+            <div className="row">
+                <div className="form-group col-4">
+                    <label htmlFor="profilePic" hidden={true}>Profile Picture</label>
+                    <img src={state.userProfilePic} alt="Profile" className="img-fluid" />
+                </div>
+                <div className="col-8">
+                    <div className="form-group">
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" className="form-control" id="name" value={state.userName} disabled={true} readOnly />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" className="form-control" id="email" value={state.userEmail} disabled={true} readOnly />
+                    </div>
+                    {/* <div className="form-group">
+                    <label htmlFor="role">Role</label>
+                    <input type="text" className="form-control" id="role" value={state.userRole} readOnly />
+                </div> */}
+                </div>
             </div>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" className="form-control" id="email" value={state.userEmail} readOnly />
+            <div className="row justify-content-center align-items-center">
+                <div className="btn col-5 p-2 mx-2 my-2 text-white custom-button">Change Profile Picture</div>
+                <div className="btn col-5 p-2 mx-2 my-2 text-white custom-button">Change Password</div>
             </div>
-            {/* <div className="form-group">
-                <label htmlFor="role">Role</label>
-                <input type="text" className="form-control" id="role" value={state.userRole} readOnly />
-            </div>
-            <div className="form-group">
-                <label htmlFor="profilePic">Profile Picture</label>
-                <img src={state.userProfilePic} alt="Profile" className="img-fluid" />
-            </div> */}
         </div>
     );
 };
