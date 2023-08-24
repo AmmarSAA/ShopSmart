672d43e86966b363f7b535b028e6f6965b0bb59d
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-08-24 22:53:09 +0500

    FIX: minor update

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,21 +1,19 @@
-/************************************
-* File Name: authenticateToken.js   *
-* Author: Ammar S.A.A               *
-* Output: Authenticate token page   *
-************************************/
+/******************************************
+* File Name: authenticateToken.js   			*
+* Author: Ammar S.A.A               			*
+* Output: Authenticate token middleware 	*
+******************************************/
 
 const jwt = require("jsonwebtoken");
 require('dotenv').config()
 
 function authenticateToken(req, res, next) {
-  // Get the token from headers, query params, cookies, etc.
   const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  // Verify the token
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({ message: "Invalid token" });
